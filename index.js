73fae84e0a4dccd4683e168d7421a1e2900743ae
wizard-jS <spraguej624@gmail.com>
2019-03-10 10:07:19 -0700

    Refactor legacy proxy to use http-middle-ware and configure to route to only my service

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+var proxy = require('http-proxy-middleware')
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(morgan('dev'));
-app.use('/scripts',express.static(path.resolve(__dirname, 'node_modules')))
+app.use('/scripts', express.static(path.resolve(__dirname, './node_modules')));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}); 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/song/:songId',express.static(path.join(__dirname, 'public')));
-
-
+// app.use('/api/music-player/*', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
+// app.use('/api/about/*', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
+app.use('/api/comments/*', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
+// app.use('/api/graphql', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
+// app.use('/api/sidebar/*', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
 });
