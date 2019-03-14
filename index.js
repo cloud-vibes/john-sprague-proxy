96c2c78f0a7bfe34171ebeb47afe31b9d554d920
wizard-jS <spraguej624@gmail.com>
2019-03-13 20:13:14 -0700

    Fix type

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use('/graphql', proxy({ target: 'http://ec2-3-91-101-162.compute-1.amazonaws
 app.use('/api/sidebar/*', proxy({ target: 'http://ec2-3-91-101-162.compute-1.amazonaws.com', changeOrigin: true }))
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public/index.html'));
-})
+});
+
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
 });
