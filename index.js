c768fd6a2acf14f655cbab9e390ad81f08e540ca
wizard-jS <spraguej624@gmail.com>
2019-03-15 14:44:32 -0700

    Add get

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ var proxy = require('http-proxy-middleware')
 const app = express();
 const port = 3000;
 
+
+app.get('/loaderio-f018b5f4ae7a56449b7525363f7b6f67/', (req, res) => { 
+	res.sendFile(path.resolve(__dirname, './loaderio-f018b5f4ae7a56449b7525363f7b6f67.txt'));
+});
+
 app.use('/scripts', express.static(path.resolve(__dirname, './node_modules')));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }); 
+
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/api/musicplayer/*', proxy({ target: 'http://54.242.57.180', changeOrigin: true }))
 // app.use('/api/description/*', proxy({ target: 'http://ec2-54-175-49-234.compute-1.amazonaws.com', changeOrigin: true }))
@@ -21,9 +27,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
-app.get('/loaderio-f018b5f4ae7a56449b7525363f7b6f67/', (req, res) => { 
-	res.sendFile(path.resolve(__dirname, './loaderio-f018b5f4ae7a56449b7525363f7b6f67.txt'));
-}); 
+ 
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
 });
